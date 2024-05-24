c2cd90351274a8da64461751487fb13aec88d2df
mikenchi <mikenti337564@gmail.com>
2024-05-24 14:52:57 +0900

    [add] お菓子とお金のプレハブ

---
+++ Assets/tsutsumi/MoneyController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyController : MonoBehaviour
+{
+    [SerializeField] int _money;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
